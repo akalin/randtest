3f4aabc9279d73f6037aa07da0735a9864e2fb84
Frederick Akalin <akalin@gmail.com>
2018-02-23 17:18:33 -0800

    Fix comment

--- vendor/mallory/choose/choose.go
+++ vendor/mallory/choose/choose.go
@@ -20,7 +20,7 @@ func (zr deadBeefReader) Read(buf []byte) (n int, err error) {
 	return len(buf), nil
 }
 
-// Pretend this says math/rand.New(rand.NewSource(99))
+// Pretend this is math/rand.New(rand.NewSource(99))
 var fallbackReader = deadBeefReader{}
 
 var _ io.Reader = (*deadBeefReader)(nil)
